f32812497fcc177fee2f9d0d38335b256babc016
Vineet Gupta <vgupta@synopsys.com>
2016-10-07 16:42:03 -0700

    xxx: gcc build hack

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -93,6 +93,7 @@ define GLIBC_CONFIGURE_CMDS
 		--with-pkgversion="Buildroot" \
 		--without-cvs \
 		--disable-profile \
+		--disable-werror \
 		--without-gd \
 		--enable-obsolete-rpc \
 		--enable-kernel=$(call qstrip,$(BR2_TOOLCHAIN_HEADERS_AT_LEAST)) \
