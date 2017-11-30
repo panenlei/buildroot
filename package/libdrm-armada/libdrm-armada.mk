14ade2eb256bf490c09c3aa44558099fe3784999
Evgeniy Didin <didin@synopsys.com>
2017-11-30 18:36:55 +0300

    Add libdrm-armada package to br, which is necessary for xf86-driver-video-armada

--- package/libdrm-armada/libdrm-armada.mk
+++ package/libdrm-armada/libdrm-armada.mk
@@ -4,16 +4,19 @@
 #
 ################################################################################
 
-LIBDRM_ARMADA_VERSION = 6b461c163b0bd02c76b65d94cc2fb3767167bda8
+LIBDRM_ARMADA_VERSION = 6b461c
 LIBDRM_ARMADA_SITE = http://git.arm.linux.org.uk/cgit/libdrm-armada.git
 LIBDRM_ARMADA_SITE_METHOD = git
-LIBDRM_LICENSE = MIT
-LIBDRM_INSTALL_STAGING = YES
-#LIBDRM_DEPENDENCIES = \
-#	libpthread-stubs \
-#	host-pkgconf
+LIBDRM_ARMADA_DEPENDENCIES = libdrm
 
-#LIBDRM_CONF_OPTS = \
+# We're patching configure.ac
+LIBDRM_ARMADA_AUTORECONF = YES
 
+# Needed for autoreconf to work properly
+define LIBDRM_ARMADA_FIXUP_M4_DIR
+        mkdir $(@D)/m4
+endef
+
+LIBDRM_ARMADA_POST_PATCH_HOOKS += LIBDRM_ARMADA_FIXUP_M4_DIR
 
 $(eval $(autotools-package))
